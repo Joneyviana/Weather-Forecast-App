9fc30cc936577680dcd2a0e72671f6fdb826328b
Joneyviana <joneyviana@gmail.com>
2020-08-06 07:33:00 -0300

    Retrofit add ApiKey interceptor

--- app/src/main/java/com/desafio/weatherforecast/data/WeatherRetrofitConfig.kt
+++ app/src/main/java/com/desafio/weatherforecast/data/WeatherRetrofitConfig.kt
@@ -1,14 +1,17 @@
 package com.desafio.weatherforecast.data
 
 import android.content.Context
+import com.desafio.weatherforecast.data.network.CacheManager
 import com.desafio.weatherforecast.data.network.hasNetwork
-import okhttp3.Cache
+import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-var OPEN_WEATHER_BASE_API = "https://api.openweathermap.org/data/2.5/"
 
+var OPEN_WEATHER_BASE_API = "https://api.openweathermap.org/data/2.5/"
+var WEATHER_API = System.getenv("WEATHER_API")
 object RetrofitConfig {
     var retrofit :Retrofit? = null
     fun getRetrofit(context: Context):Retrofit{
@@ -22,17 +25,29 @@ object RetrofitConfig {
         return retrofit!!
     }
 
+    fun getWeatherService(context: Context):WeatherService{
+        return getRetrofit(context).create(WeatherService::class.java)
+    }
+
+    private fun getHttpWithApiKey(request: Request):HttpUrl{
+        val originalHttpUrl: HttpUrl = request.url()
+        return originalHttpUrl.newBuilder()
+            .addQueryParameter("appid", WEATHER_API)
+            .build()
+    }
     private fun getOkHttpClient(context:Context):OkHttpClient{
-        val cacheSize = (5 * 1024 * 1024).toLong()
-        val myCache = Cache(context.cacheDir, cacheSize)
+        var cacheManager = CacheManager(context)
         return OkHttpClient.Builder()
-            .cache(myCache)
+            .cache(cacheManager.cache)
             .addInterceptor { chain ->
                 var request = chain.request()
+                var url = getHttpWithApiKey(request)
                 request = if (hasNetwork(context)!!)
-                    request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+                    request.newBuilder().header("Cache-Control",cacheManager.cacheHeaderOnline)
+                        .url(url).build()
                 else
-                    request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+                    request.newBuilder().header("Cache-Control",cacheManager.cacheHeaderOffline )
+                        .url(url).build()
                 chain.proceed(request)
             }
             .build()
